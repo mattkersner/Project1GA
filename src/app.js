10b6d4fb27a511694691fb93259f8e2286ac3892
matt kersner <mattkersner@gmail.com>
2016-12-15 11:17:32 -0500

    got collision logic to remove cheesypoofs when it collides with cartman

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@
 //     break;
 //     case 39:
 //       $('.catcher').stop().animate( {
-//         left: '+=50px'
+//         left: '+=30px'
 //     });
 //     break;
 //   }
@@ -33,46 +33,52 @@
 //   }
 // }
 
-// works perfectly, but pulled from stack overflow and need to fully understand what it's doing
+
 // .catcher top is always at 410
+
+// set gameboard to a variable
 let pane = $('.gameboard');
+// set cartman to a variable
 let box = $('.catcher');
+// establish limits that cartman can travel within the gameboard
 let limits = pane.width() - box.width();
+// store each keystroke
 let keyPress = {};
+// distance cartman will travel at each keypress
 let x = 4;
 
+// left and right arrow movement functionality
 function newValue(value, left, right) {
+  // if the left arrow is pressed (true), set variable n to x-coordinate and subtract the distance
   if (keyPress[left]) {
     var n = parseInt(value, 10) - x;
+  // if the right arrow is pressed (true), set variable n to x-coordinate and add the distance
   } else if (keyPress[right]) {
     var n = parseInt(value, 10) + x;
   };
-  // if (keyPress[right]) {
-  //   var n = parseInt(value, 10) + x;
-  // } else {
-  //   var n = parseInt(value, 10) - 0;
-  // }
-  //   var n = parseInt(value, 10)
-  //     - (keyPress[left] ? x : 0)
-  //     + (keyPress[right] ? x : 0);
+  // if n is less than zero, return 0 so he doesn't move beyond the 0 x-coordinate
   if (n < 0) {
     return 0;
+  // if n is greater than the established limits, return the limits so that cartman can't go beyond the game board
   } else if (n > limits) {
     return limits;
+  // if neither of those things are true, just return n to move him around
   } else {
     return n;
-    // return n < 0 ? 0 : n > limits ? limits : n;
   };
 }
 
+// when the key is pressed, grab the keycode to determine which key and set it to true so that it knows to move
 $(window).keydown(function(e) {
   keyPress[e.which] = true;
 })
 
+// when the key is released, change to false so cartman no longer moves
 $(window).keyup(function(e) {
   keyPress[e.which] = false;
 });
 
+// an interval set to look every 20 miliseconds for a new x-coordinate and set it to cartmans left css property
 setInterval(function() {
     box.css({
         left: function coordinates(y, value) {
---
+++ src/game.js
@@ -0,0 +1,9 @@
+
+    function coord() {
+      var y1 = $('.item').offset().left;
+      var y2 = $('.catcher').offset().left;
+
+      console.log(y1);
+      console.log(y2);
+ }
+
