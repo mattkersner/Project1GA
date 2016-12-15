016b481ae39cf82f632070714613147ed916ff56
matt kersner <mattkersner@gmail.com>
2016-12-14 21:08:59 -0500

    refactored my movement function for cartman

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@
 //   switch(e.which) {
 //     case 37:
 //       $('.catcher').stop().animate( {
-//         left: '-=50px'
+//         left: '-=30px'
 //     });
 //     break;
 //     case 39:
@@ -33,52 +33,50 @@
 //   }
 // }
 
-// works perfectly, but need to fully understand what it's doing
+// works perfectly, but pulled from stack overflow and need to fully understand what it's doing
+// .catcher top is always at 410
 let pane = $('.gameboard');
 let box = $('.catcher');
 let limits = pane.width() - box.width();
-let d = {};
+let keyPress = {};
 let x = 4;
 
-function newv(v, left, right) {
-    var n = parseInt(v, 10) - (d[left] ? x : 0) + (d[right] ? x : 0);
-    return n < 0 ? 0 : n > limits ? limits : n;
+function newValue(value, left, right) {
+  if (keyPress[left]) {
+    var n = parseInt(value, 10) - x;
+  } else if (keyPress[right]) {
+    var n = parseInt(value, 10) + x;
+  };
+  // if (keyPress[right]) {
+  //   var n = parseInt(value, 10) + x;
+  // } else {
+  //   var n = parseInt(value, 10) - 0;
+  // }
+  //   var n = parseInt(value, 10)
+  //     - (keyPress[left] ? x : 0)
+  //     + (keyPress[right] ? x : 0);
+  if (n < 0) {
+    return 0;
+  } else if (n > limits) {
+    return limits;
+  } else {
+    return n;
+    // return n < 0 ? 0 : n > limits ? limits : n;
+  };
 }
 
-$(window).keydown(function(e) { d[e.which] = true; })
-$(window).keyup(function(e) { d[e.which] = false; });
+$(window).keydown(function(e) {
+  keyPress[e.which] = true;
+})
+
+$(window).keyup(function(e) {
+  keyPress[e.which] = false;
+});
 
 setInterval(function() {
     box.css({
-        left: function(i,v) {
-          // console.log({i, v})
-          return newv(v, 37, 39); },
+        left: function coordinates(y, value) {
+          return newValue(value, 37, 39);
+        },
     });
 }, 20);
-
-// var pane = $('#pane'),
-//     box = $('#box'),
-//     maxValue = pane.width() - box.width(),
-//     keysPressed = {},
-//     distancePerIteration = 3;
-
-// function calculateNewValue(oldValue, keyCode1, keyCode2) {
-//     var newValue = parseInt(oldValue, 10)
-//                    - (keysPressed[keyCode1] ? distancePerIteration : 0)
-//                    + (keysPressed[keyCode2] ? distancePerIteration : 0);
-//     return newValue < 0 ? 0 : newValue > maxValue ? maxValue : newValue;
-// }
-
-// $(window).keydown(function(event) { keysPressed[event.which] = true; });
-// $(window).keyup(function(event) { keysPressed[event.which] = false; });
-
-// setInterval(function() {
-//     box.css({
-//         left: function(index ,oldValue) {
-//             return calculateNewValue(oldValue, 37, 39);
-//         },
-//         top: function(index, oldValue) {
-//             return calculateNewValue(oldValue, 38, 40);
-//         }
-//     });
-// }, 20);
